eecf8486a56c841f9ccee731935e41511f552aa3
张阳 <9744835+zhang-yang277@user.noreply.gitee.com>
2022-11-08 09:09:16 +0800

    third

--- src/main/java/com/zhang/controller/APP.java
+++ src/main/java/com/zhang/controller/APP.java
@@ -4,5 +4,6 @@ public class APP {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("world");
+        System.out.println("java");
     }
 }
