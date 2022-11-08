10a391d449039506824019e935043b8e440d532a
zhangyang277 <102478075+zhangyang277@users.noreply.github.com>
2022-11-08 10:15:11 +0800

    Update APP.java
    GitHub001

--- src/main/java/com/zhang/controller/APP.java
+++ src/main/java/com/zhang/controller/APP.java
@@ -5,7 +5,7 @@ public class APP {
         System.out.println("hello");
         System.out.println("world");
         System.out.println("java");
-        System.out.println("push");
+     
 
     }
 }
