72b48299023064b21b1f64e9d7b0aec23b5082a5
zhangyang277 <102478075+zhangyang277@users.noreply.github.com>
2022-11-08 10:20:22 +0800

    Update APP.java

--- src/main/java/com/zhang/controller/APP.java
+++ src/main/java/com/zhang/controller/APP.java
@@ -4,7 +4,7 @@ public class APP {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("world");
-        System.out.println("java");
+        System.out.println("python");
      
 
     }
